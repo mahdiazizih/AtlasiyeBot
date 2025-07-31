3a2365f9f000be311db5ecbf7e2bd4995484227b
mahdiazizih <59334885+mahdiazizih@users.noreply.github.com>
2025-07-31 13:46:10 +0330

    Update group_-100278729234.py

--- wedding_bot/groups/group_-100278729234.py
+++ wedding_bot/groups/group_-100278729234.py
@@ -1,1 +1,7 @@
+from aiogram import Router
+from core.command_router import register_common_commands  # دستورات مشترک
 
+group_-1002787279234_router = Router(name="group_-1002787279234")
+
+# ثبت دستورات مشترک برای این گروه
+register_common_commands(group_-1002787279234_router)
