399ba87ba545c8a3c2da2346ce345c69ba2c4bfb
mahdiazizih <59334885+mahdiazizih@users.noreply.github.com>
2025-07-31 12:49:52 +0330

    Update command_router.py

--- wedding_bot/core/command_router.py
+++ wedding_bot/core/command_router.py
@@ -2,11 +2,11 @@ from telegram import Update
 from telegram.ext import ContextTypes, CommandHandler
 
 # ایمپورت دستورات
-from commands import save, show, edit, dele, test  # این خط را به‌روزرسانی کن
+from commands import save, show, edit, del , test  # این خط را به‌روزرسانی کن
 
 def register_command_handlers(application):
     application.add_handler(CommandHandler("save", save.save_command))
     application.add_handler(CommandHandler("show", show.show_command))
     application.add_handler(CommandHandler("edit", edit.edit_command))
-    application.add_handler(CommandHandler("dele", delete.delete_command))
+    application.add_handler(CommandHandler("del", delete.delete_command))
     application.add_handler(CommandHandler("test", test.test_command))  # این خط را اضافه کن
