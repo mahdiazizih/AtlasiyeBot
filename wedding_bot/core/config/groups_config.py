257c7d7afca6323ddd1cf5430b8e004797f9a16b
mahdiazizih <59334885+mahdiazizih@users.noreply.github.com>
2025-07-31 13:06:28 +0330

    Update groups_config.py

--- wedding_bot/core/config/groups_config.py
+++ wedding_bot/core/config/groups_config.py
@@ -1,1 +1,14 @@
+# wedding_bot/config/groups_config.py
 
+GROUPS_CONFIG = {
+    -1002787279234: {  # گروه تالار
+        "name": "تالار اطلس",
+        "db_schema": "atlas",
+        "commands_enabled": ["test", "save", "edit", "del", "show"]
+    },
+    -4818872906: {  # گروه صندوق
+        "name": "صندوق اعتباری",
+        "db_schema": "fund",
+        "commands_enabled": ["test", "save", "show"]
+    },
+}
